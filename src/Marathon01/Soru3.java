a1175c18e155075049d0bf557f3ef688dfbf9599
Gülsu Fındık <findik.gulsu@gmail.com>
2023-02-14 18:22:49 +0300

    Marathon

--- src/Marathon01/Soru3.java
+++ src/Marathon01/Soru3.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Soru3 {
 
 	public static void main(String[] args) {
-
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Lütfen bir metin giriniz: ");
 		String metin = scanner.nextLine();
 		System.out.print("Bir harf giriniz: ");
 		char harf = scanner.next().charAt(0);
 
-		harfBul(metin, harf);
+		karakterSayici(metin, harf);
 	}
 
-	static void harfBul(String metin, char harf) {
+	static void karakterSayici(String metin, char harf) {
 		int karakterSayici = 0;
 		for (int i = 0; i < metin.length(); i++) {
 			if (metin.charAt(i) == harf) {
