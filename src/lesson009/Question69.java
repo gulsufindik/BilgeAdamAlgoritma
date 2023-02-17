2c645222777b96ccfd13e494e1ba0c80d9e4a288
Gülsu Fındık <findik.gulsu@gmail.com>
2023-02-17 17:05:56 +0300

    OOPGiris

--- src/lesson009/Question69.java
+++ src/lesson009/Question69.java
@@ -14,50 +14,66 @@ public class Question69 {
 			"Bayburt", "Karaman", "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan", "Iğdır", "Yalova", "Karabük",
 			"Kilis", "Osmaniye", "Düzce" };
 
-	// parametre olarak girilen metine göre ikinci harfinden harf ile
-	// bulunan şehirlerin ilk üç harfini büyük bastıran method
-	// "a"
-	// Bayburt -> BAY
-	// Karaman -> KAR
-
 	public static void main(String[] args) {
+
 //		Scanner scanner = new Scanner(System.in);
 //		String deger = scanner.nextLine();
 //		ilMethodu(deger.charAt(1));
 
+		// bIleBaslayanlar(iller);
+
+//		String[] sesliHarfleriSilinmis = sesliHarfleriSil();
+//
+//		for (int i = 0; i < sesliHarfleriSilinmis.length; i++) {
+//
+//			System.out.println(sesliHarfleriSilinmis[i]);
+//		}
+		
+		plakaBul(iller, "Ankara","Adana","Bursa","İzmir");
+
 	}
+
+	// parametre olarak girilen harfe göre ikinci harfi girilen harf ile
+	// bulunan şehirleri ilk üç harfini büyük bastıran methodu
+	// "a"
+	// Bayburt --> BAY
+	// Karaman --> KAR
 
 	public static void ilMethodu(char x) {
 		for (int i = 0; i < iller.length; i++) {
 			if (iller[i].charAt(1) == x) {
-				System.out.println(iller[i] + "-->" + iller[i].substring(0, 3).toUpperCase());
+				System.out.println(iller[i] + "--> " + iller[i].substring(0, 3).toUpperCase());
 			}
 		}
 	}
 
-	// B harfi ile başlayan şehirlerin ilk üç harfini alalım
+	// B harfi ile başlayan şehilerin ilk üç harfini alalım
 	// metot parametre olarak dizi alsın
-	// for each kullanalım
+	// for each kullanalaım
+	public static void bIleBaslayanlar(String[] array) {
 
-	public static void bIleBslayanlar(String[] array) {
 		for (String kelime : array) {
 			if (kelime.startsWith("B")) {
-				System.out.println(kelime + "-->" + kelime.substring(0, 3).toUpperCase());
+				System.out.println(kelime + "--> " + kelime.substring(0, 3).toUpperCase());
 			}
 		}
 	}
+
 	// Sesli harfleri silip silinmiş halini return eden bir method yazalım
-	// metot String array dönsün parametre almasın
-	// return eden arrayi mainde yazdıralım
+	// metot String array döncek paramtre almasın
+	// retrun eden arrayi mainde yazdıralım
 
 	public static String[] sesliHarfleriSil() {
 		String[] sesliHarfler = { "a", "e", "ı", "i", "o", "ö", "u", "ü" };
 		String[] yeniDizi = new String[iller.length];
+		
 		for (int i = 0; i < iller.length; i++) {
 			for (int j = 0; j < sesliHarfler.length; j++) {
+				
+				if (iller[i].toLowerCase().contains(sesliHarfler[j]) ) {
 
-				if (iller[i].toLowerCase().contains(sesliHarfler[j])) {
 					iller[i] = iller[i].toLowerCase().replace(sesliHarfler[j], "");
+					
 				}
 			}
 			yeniDizi[i] = iller[i];
@@ -65,32 +81,22 @@ public class Question69 {
 		return yeniDizi;
 
 	}
-	// plakaBul(iller,"Ankara","Karaman");
-	// parametre olarak array ve sınırsız sayıda String olarak şehir alsın
-	// girilen şehirlerin plakalarını yazdırsın
+	//plakaBul(iller,"Ankara","Karaman");
+	//parametre olarak array ve sınırsız sayıda String olarak şehir alsın
+	//girilen şehirlerin plakalarını yazdırsın
 	
-	public static void plakaBul(String[] array, String...strings il) {
+	public static void plakaBul(String[] array, String... il ) {
 		for (int i = 0; i < array.length; i++) {
-			for (int j= 0; j< il.length; j++) {
+			for (int j = 0; j < il.length; j++) {
 				if(array[i].equals(il[j])) {
-					if(i<9) {
-						System.out.println("0"+(i+1)+"-"+ array[i]);
-					} else {
+					
+					if(i<=9) {
+						System.out.println("0" +(i+1) + "-" + array[i]);
+					}else {
+						System.out.println((i+1)+ "-" + array[i]);
+					}
 				}
 			}
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
