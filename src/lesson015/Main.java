27fad26860cc055a8e20b22bc549c67743ec6dbc
Gülsu Fındık <findik.gulsu@gmail.com>
2023-02-27 17:28:11 +0300

    abstract

---
+++ src/lesson015/Main.java
@@ -0,0 +1,20 @@
+package lesson015;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Mülakatta soruldu
+		// Abstract sınıflar ve interface farkı
+
+		// interfacelerde java 8 ile birlikte static keywordu sayesinde 
+		// gövdeli method yazabiliyoruz
+		// interfacelerde java 8 ile birlikte default keywordu sayesinde 
+		// gövdeli method yazabiliyoruz
+
+		// Polymophism
+		// Bir nesenin farklı nesnelerinin referanslarını tutabilme özelliği
+
+	}
+
+}
